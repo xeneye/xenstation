268bc7eaeb199637f3bacbe84ee685f68d782ff7
Xeneye <paul@xeneye.com>
2026-09-25 14:31:13 +0100

    Fresh Update

--- hardware-configuration.nix
+++ hardware-configuration.nix
@@ -14,24 +14,24 @@
   boot.extraModulePackages = [];
 
   fileSystems."/" = {
-    device = "/dev/disk/by-uuid/e7230b02-8bdb-4490-99dd-3916ebedc5cb";
+    device = "/dev/disk/by-uuid/29c6af7c-209c-47df-901e-1334374f94ab";
     fsType = "btrfs";
   };
 
   fileSystems."/home" = {
-    device = "/dev/disk/by-uuid/e7230b02-8bdb-4490-99dd-3916ebedc5cb";
+    device = "/dev/disk/by-uuid/29c6af7c-209c-47df-901e-1334374f94ab";
     fsType = "btrfs";
     options = ["subvol=home"];
   };
 
   fileSystems."/nix" = {
-    device = "/dev/disk/by-uuid/e7230b02-8bdb-4490-99dd-3916ebedc5cb";
+    device = "/dev/disk/by-uuid/29c6af7c-209c-47df-901e-1334374f94ab";
     fsType = "btrfs";
     options = ["subvol=nix"];
   };
 
   fileSystems."/boot" = {
-    device = "/dev/disk/by-uuid/8054-5788";
+    device = "/dev/disk/by-uuid/505D-0311";
     fsType = "vfat";
     options = ["fmask=0077" "dmask=0077"];
   };
